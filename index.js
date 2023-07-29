8461694277e022ff5773f5a090565970c9cce3c8
Engraya <ahmadyakubuaya@gmail.com>
2023-07-29 09:45:49 +0100

    Comments

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+// initializes teh expresss framework module and the path module
 const express = require("express");
 const path = require("path");
 
-
-
+// creating the express application
 const app = express();
 
+// joining the created views folder and the ejs view engine to the directory
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
+// set up the request response cycle with the template view render and variables
 app.get("/", (request, response) => {
-    // response.send("<h1>Hello, This is my First Node Application<h1>")
     const target = "Full stack MERN and DevOps Engineer";
     const name = "Ahmad";
     const nationality = "Nigeria";
@@ -17,7 +19,7 @@ app.get("/", (request, response) => {
     response.render("index", {target:target, name:name, nationality:nationality, age:age});
 });
 
-
+// create a server to listen to a specified port host 
 app.listen(4000, () => {
     console.log('Server Ruuning at port 4000......');
 
